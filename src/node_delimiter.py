e6069dc978ca7b84813634bb3bf5aaa9d2e7a4e8
TotesNasty <ericwhite19521@gmail.com>
2025-03-08 21:43:22 -0600

    added block breakdown and block tagging

--- src/node_delimiter.py
+++ src/node_delimiter.py
@@ -1,6 +1,18 @@
 from enum import Enum
 from htmlnode import HTMLNode, LeafNode, ParentNode
 from textnode import TextNode, TextType
+from split_nodes import split_nodes_image, split_nodes_link
+
+
+def text_to_nodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
+
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
--- src/test_node_delimiter.py
+++ src/test_node_delimiter.py
@@ -1,6 +1,7 @@
 import unittest
 from textnode import TextNode, TextType
-from node_delimiter import split_nodes_delimiter
+from node_delimiter import split_nodes_delimiter, text_to_nodes
+
 
 class TestNodeDelimiter(unittest.TestCase):
     def test_no_delimiter(self):
@@ -33,3 +34,23 @@ class TestNodeDelimiter(unittest.TestCase):
         ]
         result = split_nodes_delimiter(nodes, "**", TextType.BOLD)
         self.assertEqual(len(result), 4)
+
+    def test_text_to_textnode(self):
+        nodes= text_to_nodes(
+                "This is **text** with an _italic_ word and a `code block` and an ![image](https://i.imgur.com/zjjcJKZ.png) and a [link](https://boot.dev)"
+            )
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            nodes,
+        )
